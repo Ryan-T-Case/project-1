5c85d38b82578eae5a321730a12efa776afb729f
Alex <38813232+aosilvester@users.noreply.github.com>
2018-09-22 12:43:57 -0400

    will this work? who knows

--- javascript/app.js.js
+++ javascript/app.js.js
@@ -43,7 +43,7 @@ var number = 0;
 
 //Function for adding selected recipe to window for review
 function populateRecipeWindow() {
-    $(document).on("click", ".1", function() {
+    $(document).on("click", ".1", function () {
         $("#recipeWindow").html(`
         <h2>Pizza</h2>
         `)
@@ -54,7 +54,7 @@ function populateRecipeWindow() {
 //Onclick event for buttons
 
 //Chicken Button
-$("#chicken").click(function() {
+$("#chicken").click(function () {
     //Clear the search results before adding anything
     $(".searchResults").empty();
     //Loop through the chicken results array and add recipe data each time
@@ -68,7 +68,7 @@ $("#chicken").click(function() {
 });
 
 //Pork Button
-$("#pork").click(function() {
+$("#pork").click(function () {
     //Clear the search results before adding anything
     $(".searchResults").empty();
     //Loop through the chicken results array and add recipe data each time
@@ -78,3 +78,60 @@ $("#pork").click(function() {
         `)
     };
 });
+
+
+// google maps code below
+// locations are hard coded in for this version. placed by coordinates
+var locations = [
+    ['Libbie Market', 37.5746, -77.5182],
+    ['Kroger Willow Lawn', 37.5802, -77.4991],
+    ['Aldi West Broad', 37.5958, -77.513137],
+    ['Lidl West Broad', 37.579832, -77.490471],
+    ['Nicks International Foods', 37.574379, -77.481691],
+    ['Stellas Grocery - Monument Location', 37.56731, -77.486151],
+    ['Ellwood Thompsons', 37.557447, -77.488621],
+    ['Kroger Carytown', 37.554478, -77.488031],
+    // ['Kroger Parham', 37.603089, -77.561696],
+    // ['Kroger Short Pump', 37.652874, -77.625489],
+    // ['grocery', 3333, 33333],
+    // ['grocery', 3333, 33333],
+    // ['grocery', 3333, 33333],
+    // ['grocery', 3333, 33333],
+    // ['grocery', 3333, 33333],
+    // ['grocery', 3333, 33333],
+    // ['grocery', 3333, 33333],
+    // ['grocery', 3333, 33333],
+    // ['grocery', 3333, 33333],
+    // ['grocery', 3333, 33333],
+    // ['grocery', 3333, 33333],
+    // ['grocery', 3333, 33333],
+    // ['grocery', 3333, 33333],
+    // ['grocery', 3333, 33333],
+
+    // https://www.gps-coordinates.net/ is a lifesaver.
+];
+console.log(locations);
+var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center: new google.maps.LatLng(37.5759, -77.5410),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+});
+//   console.log(map);
+var infowindow = new google.maps.InfoWindow();
+// console.log(infowindow);
+var marker, i;
+for (i = 0; i < locations.length; i++) {
+    marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i]
+            [2]),
+        map: map
+    });
+    google.maps.event.addListener(marker, 'click', (function (marker, i) {
+        return function () {
+            infowindow.setContent(locations[i][0]);
+            infowindow.open(map, marker);
+        }
+    })(marker, i));
+};
+
+// end of google maps code
